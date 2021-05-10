36eed33de649c722d56b153b36a2cb3cc1b5cc87
ajayKumar99 <ajax99k@gmail.com>
2021-05-10 22:03:27 +0530

    Image prediction fixes

--- imageHandler.py
+++ imageHandler.py
@@ -13,9 +13,11 @@ IMG_HEIGHT = 512
 IMG_CHANNELS = 3
 
 s_model = get_segmentation_model()
+d_model = get_detection_model()
 
 def image_upload_handler(img_data):
     img = cv2.imdecode(np.frombuffer(img_data, np.uint8), cv2.IMREAD_UNCHANGED)
+    img1 = cv2.imdecode(np.frombuffer(img_data, np.uint8), cv2.IMREAD_UNCHANGED)
     frame = cv2.resize(img, (IMG_HEIGHT, IMG_WIDTH))
     inp = tf.keras.preprocessing.image.img_to_array(frame)
     res = s_model.predict(np.expand_dims(inp, axis=0))
@@ -36,4 +38,7 @@ def image_upload_handler(img_data):
     img2.save(rawBytes2, "JPEG")
     rawBytes2.seek(0)
     img2_base64 = base64.b64encode(rawBytes2.read())
-    return jsonify({'status':img_base64.decode("utf-8"), 'inp': img2_base64.decode("utf-8")})
+    frame = cv2.resize(img1, (256, 256))
+    inp = tf.keras.preprocessing.image.img_to_array(frame) / 255.0
+    res = d_model.predict(np.expand_dims(inp, axis=0))
+    return jsonify({'status':img_base64.decode("utf-8"), 'inp': img2_base64.decode("utf-8"), 'labels': res.tolist()})
